6a5b8783e2664d403657947086fe81c56fd3a531
sangeeha-2005 <sangeetha09005@gmail.com>
2025-11-26 11:17:37 +0530

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to Full Stack Development Project</h1>
+      <p>This is the frontend React interface.</p>
+    </div>
+  );
+}
+
+export default App;
